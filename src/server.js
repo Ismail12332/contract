eede6b89a4798f3ae81ebaee447c3622a2052d25
Ismail12332 <i6836328@gmail.com>
2025-05-06 18:36:38 +0700

    modified:   src/server.js

--- src/server.js
+++ src/server.js
@@ -65,6 +65,10 @@ const checkJwt = jwt({
     algorithms: ['RS256'],
 });
 
+server.listen(PORT, () => {
+    console.log(`✅ Server is running on port ${PORT}`);
+});
+
 // Настройка CORS
 app.use(cors({
     origin: 'https://contractors.onrender.com', // Укажите адрес вашего фронтенда
